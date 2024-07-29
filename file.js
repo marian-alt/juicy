758271694ab03a432e0fb489ddae7a659748cf1f
marian-alt <odafenmarian28@gmail.com>
2024-07-29 12:57:04 +0100

    first commit

---
+++ file.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".fa-bars");
+const signup = document.querySelector(".signUpContainer");
+
+btn.addEventListener("click", function(){
+    signup.style.display = "block";
+})
